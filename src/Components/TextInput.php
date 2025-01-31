19203f69b438b6123d3d337491520ebfee718f2f
Gianluca Mannai <g.mannai@invisiblefarm.com>
2025-01-31 15:19:14 +0100

    First commit

---
+++ src/Components/TextInput.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Akosnoavek\LaravelFormComponents\Components;
+class TextInput
+{
+    public function render(): string
+    {
+        return view('laravel-form-components::text-input');
+    }
+}
---
+++ src/Support/ServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+use \Illuminate\Support\ServiceProvider as BaseServiceProvider;
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../../Config/config.php' => config_path('form-components.php'),
+            ], 'config');
+
+            $this->publishes([
+                __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/form-components'),
+            ], 'views');
+        }
+
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'form-components');
+
+//        //
+//
+//        Blade::directive('bind', function ($bind) {
+/*            return '<?php app(\ProtoneMedia\LaravelFormComponents\FormDataBinder::class)->bind(' . $bind . '); ?>';*/
+//        });
+//
+//        Blade::directive('endbind', function () {
+/*            return '<?php app(\ProtoneMedia\LaravelFormComponents\FormDataBinder::class)->pop(); ?>';*/
+//        });
+//
+//        Blade::directive('wire', function ($modifier) {
+/*            return '<?php app(\ProtoneMedia\LaravelFormComponents\FormDataBinder::class)->wire(' . $modifier . '); ?>';*/
+//        });
+//
+//        Blade::directive('endwire', function () {
+/*            return '<?php app(\ProtoneMedia\LaravelFormComponents\FormDataBinder::class)->endWire(); ?>';*/
+//        });
+
+        //
+
+        $prefix = config('form-components.prefix');
+
+        Collection::make(config('form-components.components'))->each(
+            fn ($component, $alias) => Blade::component($alias, $component['class'], $prefix)
+        );
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../../Config/config.php', 'form-components');
+
+//        $this->app->singleton(FormDataBinder::class, fn () => new FormDataBinder);
+    }
+}
