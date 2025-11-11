a4b73e26c7754b9d40da9bab7ef6f6be5c0e9e38
AkosNoavekCode <g.mannai@invisiblefarm.com>
2025-11-11 11:11:45 +0100

    refactor

--- src/Components/FormInput.php
+++ src/Components/FormInput.php
@@ -4,13 +4,12 @@ namespace Akosnoavek\LaravelFormComponents\Components;
 
 use AkosNoavek\LaravelFormComponents\Components\Component;
 
-class TextInput extends Component
+class FormInput extends Component
 {
-    public function __construct
-    (
+    public function __construct(
         public string $name,
         public string $label
-    ){
+    ) {
         $this->id = $this->id();
     }
 }
