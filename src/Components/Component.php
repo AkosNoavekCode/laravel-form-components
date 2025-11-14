3bc48226cc22f8d25e89d84d6ba774ba95fd78f7
g.mannai <g.mannai@invisiblefarm.com>
2025-11-14 18:06:59 +0100

    Add form data binder

--- src/Components/Component.php
+++ src/Components/Component.php
@@ -8,16 +8,26 @@ use Illuminate\View\Component as BaseComponent;
 
 class Component extends BaseComponent
 {
-    public string $id;
-    public string $name = '';
+  public string $id;
 
-    public function render()
-    {
-        return view(config("form-components.components.".Str::kebab(class_basename($this::class)) .".view"));
+  public string $name = '';
+
+  public function isWired(): bool
+  {
+    if ($this->attributes && count($this->attributes->whereStartsWith('wire:model')->getIterator())) {
+      return false;
     }
 
-    public function id()
-    {
-        return base64_encode(random_int(0, 9999) . isset($this->name) ? $this->name : null);
-    }
+    return app(FormDataBinder::class)->isWired();
+  }
+
+  public function render()
+  {
+    return view(config("form-components.components." . Str::kebab(class_basename($this::class)) . ".view"));
+  }
+
+  public function id()
+  {
+    return base64_encode(random_int(0, 9999) . isset($this->name) ? $this->name : null);
+  }
 }
--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -8,38 +8,54 @@ use \Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
-    public function boot(): void
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../../Config/config.php' => config_path('form-components.php'),
-            ], 'config');
+  /**
+   * Bootstrap the application services.
+   */
+  public function boot(): void
+  {
+    if ($this->app->runningInConsole()) {
+      $this->publishes([
+        __DIR__ . '/../../Config/config.php' => config_path('form-components.php'),
+      ], 'config');
 
-            $this->publishes([
-                __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/form-components'),
-            ], 'views');
-        }
-
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'form-components');
-
-
-        $prefix = config('form-components.prefix');
-
-        Collection::make(config('form-components.components'))->each(
-            fn($component, $alias) => Blade::component($alias, $component['class'], $prefix)
-        );
+      $this->publishes([
+        __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/form-components'),
+      ], 'views');
     }
 
-    /**
-     * Register the application services.
-     */
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../../Config/config.php', 'form-components');
+    $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'form-components');
 
-        //        $this->app->singleton(FormDataBinder::class, fn () => new FormDataBinder);
-    }
+    Blade::directive('bind', function ($bind) {
+      return '<?php app(\AkosNoavek\LaravelFormComponents\FormDataBinder::class)->bind(' . $bind . '); ?>';
+    });
+
+    Blade::directive('endbind', function () {
+      return '<?php app(\AkosNoavek\LaravelFormComponents\FormDataBinder::class)->pop(); ?>';
+    });
+
+    Blade::directive('wire', function ($modifier) {
+      return '<?php app(\AkosNoavek\LaravelFormComponents\FormDataBinder::class)->wire(' . $modifier . '); ?>';
+    });
+
+    Blade::directive('endwire', function () {
+      return '<?php app(\AkosNoavek\LaravelFormComponents\FormDataBinder::class)->endWire(); ?>';
+    });
+
+
+    $prefix = config('form-components.prefix');
+
+    Collection::make(config('form-components.components'))->each(
+      fn($component, $alias) => Blade::component($alias, $component['class'], $prefix)
+    );
+  }
+
+  /**
+   * Register the application services.
+   */
+  public function register(): void
+  {
+    $this->mergeConfigFrom(__DIR__ . '/../../Config/config.php', 'form-components');
+
+    $this->app->singleton(FormDataBinder::class, fn() => new FormDataBinder);
+  }
 }
