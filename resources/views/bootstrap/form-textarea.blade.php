2c4793a28fbcb81a5a8acd81213b4d72f6d4a543
Noavek <AkosNoavek@proton.me>
2025-11-19 11:26:17 +0100

    Update textarea styles with border-radius

--- resources/views/bootstrap/form-textarea.blade.php
+++ resources/views/bootstrap/form-textarea.blade.php
@@ -1,6 +1,6 @@
 <div>
     <label for="{{ $id }}">{{ $label }}</label>
-    <textarea wire:model="{{$name}}" class="form-control" id="{{ $id }}" style="border: 1px solid lightgray; height: 200px" {!! $attributes !!}></textarea>
+    <textarea wire:model="{{$name}}" class="form-control" id="{{ $id }}" style="border-radius: 10px; border: 1px solid lightgray; height: 200px" {!! $attributes !!}></textarea>
     @error($name)
       <span>{{$message}}</span>
     @enderror
