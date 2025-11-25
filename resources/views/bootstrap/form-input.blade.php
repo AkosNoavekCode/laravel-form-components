099c26865df964842c4faf50b340c9e959cfd52b
Noavek <AkosNoavek@proton.me>
2025-11-25 10:24:31 +0100

    Add name attribute to input field in form

--- resources/views/bootstrap/form-input.blade.php
+++ resources/views/bootstrap/form-input.blade.php
@@ -1,7 +1,7 @@
 <div wire:key={{$id}}>
     <div class="mb-3">
         <label for="{{ $id }}">{{ $label }}</label>
-        <input style="border: 1px solid lightgray; border-radius: 10px" class="form-control" id="{{ $id }}" wire:model="{{$name}}"  {!! $attributes !!}/>
+        <input name="{{$name}}" style="border: 1px solid lightgray; border-radius: 10px" class="form-control" id="{{ $id }}" wire:model="{{$name}}"  {!! $attributes !!}/>
         @error($name)
             <span class="error">{{$message}}</span>
         @enderror
