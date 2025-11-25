27a19a1651c4fadce4dc7494cdbd6e1137138ac4
Noavek <AkosNoavek@proton.me>
2025-11-25 14:49:19 +0100

    Refactor form-group layout for inline and block styles

--- resources/views/bootstrap/form-group.blade.php
+++ resources/views/bootstrap/form-group.blade.php
@@ -1,7 +1,15 @@
 <div>
     <label>{{$label}}</label>
 
-    <div class="@if($inline) d-flex flex-row flex-wrap inline-space @endif" {!! $attributes !!}>
+    <div
+        @class([
+            "d-flex flex-row flex-wrap inline-space" => (bool) $inline,
+            "d-flex flex-column flex-wrap inline-space g-2" => ! (bool) $inline
+        ])
+        @style([
+            "gap: 10px" => ! (bool) $inline
+        ])
+        {!! $attributes !!}>
         {!! $slot !!}
     </div>
 </div>
